ad42335860ba2c1e161ce5bbff87fbf303bc5548
amurahovskiy <51996639+amurahovskiy@users.noreply.github.com>
2020-04-12 23:30:29 +0300

    Update exapmple.cpp

--- exapmple.cpp
+++ exapmple.cpp
@@ -1,52 +1,71 @@
-
 #include <iostream>
+#include <cstdio> 
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#define SIZE sizeof (int)*8
 
 using namespace std;
 
-
-void View(int **arr, int row, int col) {
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < col; j++) {
-            printf("%3d", arr[i][j]);
+double **FillMatrica (int n)//������ ��������� ������� �
+{
+	double **mA;
+	mA= new double *[n];
+	new int [n];
+		for (int i=0; i<n; i++)
+			mA[i]=new double[n];
+	for (int i=0; i<n; i++)
+    	{
+		for (int j=0; j<n; j++)
+    		{
+				mA[i][j]= rand()%20;
+        	}
         }
-        printf("\n");
-    }
+	return mA;
+}
+double **FillMatricaB (int n)//�� ����� �������� ������ ������� �
+	{
+	double **mA;
+	mA = new double *[n];
+		for (int i=0; i<n; i++)
+			mA[i]=new double[n];
+	for (int i=0; i<n; i++)
+    	{
+		for (int j=0; j<n; j++)
+    		{
+        		mA[i][j] = 1.0/(i+j+1.0);
+        	}
+        }
+	return mA;
 }
 
-int main() {
-    int row=1000, col=1000;
-    
-
-    // Выделение памяти под массив
-    int **arr = (int **)malloc(row * sizeof(int *));
-    for(int i = 0; i < row; i++)
-        arr[i] = (int *)malloc(col * sizeof(int));
-
-    // Присваивание случайных значений
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < col; j++) {
-            arr[i][j] = (int) rand() % 100;
-        }
-    }
+double **MultiplyMatrica (int n, double**c, double **z)//������ ������� �������
+	{
+	double **mA;
+	mA = new double *[n];
+		for (int i=0; i<n; i++)
+			mA[i]=new double[n];
+	for(int i = 0; i < n; i++)
+    		for(int j = 0; j < n; j++){
+      			mA[i][j] = 0;
+      			for(int k = 0; k < n; k++)
+       			 mA[i][j] += z[i][k]*c[k][j];}
+	return mA;
+}
 
 
-    View(arr, row, col);
 
-    int MinRow, Temp;
-    for (int NumCol = 0; NumCol < col; NumCol++) {
-        for (int NumRow = 0; NumRow < row - 1; NumRow++) { 
-            MinRow = NumRow;
-            for (int j = NumRow + 1; j < row; j++)
-                if (arr[j][NumCol] > arr[MinRow][NumCol])
-                   MinRow = j;
-            Temp = arr[NumRow][NumCol];
-            arr[NumRow][NumCol] = arr[MinRow][NumCol]; 
-            arr[MinRow][NumCol] = Temp;
-        }
-    }
+int main()
+{	
+	
+	int number=500;
+	double **z =FillMatrica(number);
 
-    printf("\n");
-    View(arr, row, col);
+
+	double **c=FillMatricaB(number);
+
+
+	double **q = MultiplyMatrica(number,c,z);
 
 
 }
